1ac63146b0b4391601752d07fc842feb1edcd25a
Soubhagya <soubhagyashankar27@gmail.com>
2023-11-29 05:31:00 -0500

    commit

--- Sudoku-frontend/src/App.jsx
+++ Sudoku-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Board from './components/Board/Board'; // Make sure the path is correct based on your file structure
-import './App.css'; // Ensure App.css is in the same directory as App.js
+import Board from './components/Board/Board'; 
+import './App.css';
 
 function App() {
   return (
