cb234688937a7f050691a229b2c9af309610c13f
Nashrah <121132577+nashrahhaque@users.noreply.github.com>
2023-12-15 22:39:32 -0500

    edited to handle pausing for the timer

--- Sudoku-frontend/src/components/Controls/Timer.jsx
+++ Sudoku-frontend/src/components/Controls/Timer.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
+      if (!isPaused) {
+        setTime((prevTime) => prevTime + 1);
+      }
     }, 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [isPaused]);
 
   const formatTime = (time) => {
     const minutes = Math.floor(time / 60);
@@ -17,9 +20,15 @@ const Timer = () => {
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   };
 
+  const handlePauseToggle = () => {
+    setIsPaused((prevIsPaused) => !prevIsPaused);
+  };
+
   return (
     <div className="timer">
-      <span className='timer-label'>Timer: </span> <span className='timer-value'>{formatTime(time)}</span>
+      <span className='timer-label'>Timer: </span>
+      <span className='timer-value'>{formatTime(time)}</span>
+      <button onClick={handlePauseToggle}>{isPaused ? 'Resume' : 'Pause'}</button>
     </div>
   );
 };
